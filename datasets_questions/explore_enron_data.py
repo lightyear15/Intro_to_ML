df2b2151a16387dccedcf261d33ae1c6a8345aaf
lightyear15 <lightyear@teletu.it>
2015-06-02 11:26:17 +0200

    moving on
    - clean code for dataset question
    - starting answer first question second part

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -24,21 +24,28 @@ pp = pprint.PrettyPrinter()
 
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
 
-print len(enron_data)
+print "length of the dataset: " + str(len(enron_data))
+print "\n\n"
 
+print "item list for MARTIN AMANDA K" 
 print enron_data["MARTIN AMANDA K"]
 for k in enron_data["MARTIN AMANDA K"]:
     print k + ": " + str(enron_data["MARTIN AMANDA K"][k])
 print len(enron_data["MARTIN AMANDA K"])
+print "\n\n"
 
 pois = [person for (person,feature) in enron_data.items() if feature["poi"] == True]
-print len(pois)
+print "number of pois: " + str(len(pois))
+print "\n\n"
 
-print enron_data["PRENTICE JAMES"]["total_stock_value"]
+print "prentice tot stock: " + str(enron_data["PRENTICE JAMES"]["total_stock_value"])
+print "\n\n"
 
-print enron_data["COLWELL WESLEY"]["from_this_person_to_poi"]
+print "colwell mails to poi count: " + str(enron_data["COLWELL WESLEY"]["from_this_person_to_poi"])
+print "\n\n"
 
-print enron_data["SKILLING JEFFREY K"]["exercised_stock_options"]
+print "skilling ex stock options: " + str(enron_data["SKILLING JEFFREY K"]["exercised_stock_options"])
+print "\n\n"
 
 for k in enron_data.keys():
     if "LAY" in k:
@@ -49,17 +56,21 @@ for k in enron_data.keys():
 for k in enron_data.keys():
     if "FASTOW" in k:
         print k + " got " + str(enron_data[k]["total_payments"])
+print "\n\n"
 
-
-print len([person for (person,feature) in enron_data.items() if feature["salary"] != "NaN" ])
-print len([person for (person,feature) in enron_data.items() if feature["email_address"] != "NaN" ])
+print "salary entries missing: " + str(len([person for (person,feature) in enron_data.items() if feature["salary"] != "NaN" ]))
+print "mail address entries missing: " + str(len([person for (person,feature) in enron_data.items() if feature["email_address"] != "NaN"]))
+print "\n\n"
 
 ll = len(enron_data)
 notp = len([person for (person,feature) in enron_data.items() if feature["total_payments"] == "NaN" ])
-print float(float(notp)/float(ll) * 100)
+print "tot_payments entries missing: " + str(notp)
+print "percentage tot_payments entries missing on total: " + str(float(float(notp)/float(ll) * 100))
+print "\n\n"
 
 llpoi = len([person for (person,feature) in enron_data.items() if feature["poi"] == True ])
 notppoi = len([person for (person,feature) in enron_data.items() if (feature["poi"] == True) & (feature["total_payments"] == "NaN") ])
-print llpoi
-print notppoi
-print float(float(notppoi)/float(llpoi) * 100)
+print "pois: " + str(llpoi)
+print "pecentage tot_pay entries missing on poi count: " + str(float(float(notppoi)/float(llpoi) * 100))
+print "\n\n"
+
